e0a98f9bc090ec242748759e295423ce1c92ee2a
Hashcode <hashcode0f@gmail.com>
2012-10-24 23:58:11 -0700

    cleanup compiler warning
    Change-Id: I585a75665f16c4547e3fb9f16a68bd0ae19bf067

--- splashmenu/minui/events.c
+++ splashmenu/minui/events.c
@@ -34,9 +34,9 @@
 #define VIBRATOR_TIMEOUT_FILE	"/sys/class/timed_output/vibrator/enable"
 #define VIBRATOR_TIME_MS    50
 
-#define SYN_REPORT          0x00
-#define SYN_CONFIG          0x01
-#define SYN_MT_REPORT       0x02
+//#define SYN_REPORT          0x00
+//#define SYN_CONFIG          0x01
+//#define SYN_MT_REPORT       0x02
 
 #define ABS_MT_POSITION     0x2a /* Group a set of X and Y */
 #define ABS_MT_AMPLITUDE    0x2b /* Group a set of Z and W */
